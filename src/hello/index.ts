1f8eb8436ec9e03687cb9bfbf7a72e297037c4a2
娜一抹惊鸿 <18610649031@163.com>
2021-09-30 10:52:58 +0800

    style: 添加入口文件注释

--- src/hello/index.ts
+++ src/hello/index.ts
@@ -5,6 +5,17 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 // per file.
 export function hello(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    console.log('hello');
+    
     return tree;
   };
 }
+
+/**
+ * 1. hello() 是原理图入口函数，是一个规则工厂(RuleFactory),返回一个创建规则(Rule)的高阶函数,其参数是使用此原理图输入的配置
+ * 2. Rule：定义一个函数，接收一个Tree进行变换，返回新的Tree
+ * 3. Tree: 虚拟文件系统，就是文件，包含已经存在的文件(base)和需要应用到文件的变更(staging)两部分；
+ *          变化过程中，只是把需要修改的内容添加到了stagting，并没有改变base.
+ * 4. SchematicContext: 原理图上下文，每个原理图都在自己的上下文中运行
+ * 
+ */
